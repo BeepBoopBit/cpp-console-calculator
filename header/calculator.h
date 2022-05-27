2d0ddda10d0e6d95c41e4efadaf17477b0086906
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-05-28 06:50:09 +0800

    Working Expression Calculator

--- header/calculator.h
+++ header/calculator.h
@@ -6,7 +6,7 @@
 #include <stack>
 #include <queue>
 #include <vector>
-
+#include <algorithm>
 
 // No need to be separated by spaces
 
@@ -18,18 +18,255 @@
  * 4.) Solve it using stack
  */
 
+class BinaryTree{
+
+private:
+    struct Node{
+       std::string value;
+       Node *left;
+       Node *right;
+       Node *parent; 
+    };
+
+public:
+    BinaryTree(){
+        _head = nullptr;
+    }
+
+    void pushValue(std::string value){
+
+        Node *tempNode = _head;
+        // Go as far right as possible
+        while(tempNode->right != nullptr){
+            tempNode = tempNode->right;
+        }
+
+        // if it's not an operator
+        if(!isOperatorStr(tempNode->value)){
+            tempNode = traverseFindOperator(tempNode);
+        }
+        
+        if(tempNode->left == nullptr){
+            tempNode->left = createNewNode(value, tempNode);
+        }else{
+            tempNode->right = createNewNode(value, tempNode);
+        }
+    }
+    
+    // assumes that this will be the first method to be called
+    void pushOperator(std::string opValue){
+        
+        if(_head == nullptr){
+            _head = createNewNode(opValue);
+        }else{
+            Node *tempNode = _head;
+            // go as far right as possible
+            while(tempNode->right != nullptr){
+                tempNode = tempNode->right;
+            }
+            // if it's not an operator then it means we can't put it on the right side
+            if(!isOperatorStr(tempNode->value)){
+                tempNode=traverseFindOperator(tempNode);
+            }
+            if(tempNode->right == nullptr){
+                tempNode->right = createNewNode(opValue, tempNode);
+            }else{
+                tempNode->left = createNewNode(opValue, tempNode);
+            }
+        }
+    }
+
+    void pushEqual(std::string equalValue){
+        _head = createNewNode(equalValue, nullptr, _head);
+    }
+
+
+    int solve(){
+        // Traverse far right until the next value is not an operator
+        Node *tempHead = _head;
+        while(isOperatorStr(tempHead->right->value)){
+            tempHead = tempHead->right;
+        }
+        auxillarySolve(tempHead);
+        return std::stoi(_head->value);
+    }
+
+    void print(){
+        Node *tempNode = _head;
+        printRec(tempNode);
+    }
+
+    void erase(){
+        delete _head;
+        _head = nullptr;
+    }
+
+private:
+    
+    void auxillarySolve(Node *node){
+        // then solve with it
+        int left = 0, right = 0;
+        bool isLeft = false, isRight = false;
+
+        if(node->left == nullptr && node->right == nullptr){
+            return;
+        }
+
+        if(isDigit(node->left->value.back())){
+            left = std::stoi(node->left->value);
+            isLeft = true;
+        }
+        // check if left is an operator
+        else if(isOperatorStr(node->left->value)){
+            auxillarySolve(node->left);
+            return;
+        }
+
+        if(isDigit(node->right->value.back())){
+            right = std::stoi(node->right->value);
+            isRight = true;
+        }else{
+            auxillarySolve(node->right);
+        }
+
+        if(isLeft && isRight){
+            node->value = std::to_string(solveValue(node->value, left, right));
+
+            // Delete Left Node;
+            delete node->left;
+            node->left = nullptr;
+
+            // Delete right Node;
+            delete node->right;
+            node->right = nullptr;
+        }
+
+        if(node->parent != nullptr){
+            auxillarySolve(node->parent);
+        }
+    }
+
+    bool isDigit(char value){
+        switch (value)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                return true;
+                break;
+        }
+        return false;
+    }
+
+    void printRec(Node *node){
+        std::cout << node->value << ' ';
+        if(node->left != nullptr){
+            printRec(node->left);
+        }
+        if(node->right != nullptr){
+            printRec(node->right);
+        }
+    }
+
+    Node *traverseFindOperator(Node *node){
+        if((node->left == nullptr || node->right == nullptr) && isOperatorStr(node->value)){
+            return node;
+        }else if(isOperatorStr(node->value)){
+            if(node->left != nullptr && isOperatorStr(node->left->value)){
+                // since we need to check for right after checking left, we'll create a separate fuction for it
+                return traverseFindOperatorLeft(node->left);
+            }else{
+                return traverseFindOperator(node->parent);
+            }
+        }
+        else if(!isOperatorStr(node->value)){
+            return traverseFindOperator(node->parent);
+        }
+        return node;
+    }
+
+    Node *traverseFindOperatorLeft(Node *node){
+        if(node->right != nullptr && isOperatorStr(node->right->value)){
+            return traverseFindOperatorLeft(node->right);
+        }else if(node->left != nullptr && isOperatorStr(node->left->value)){
+            return traverseFindOperator(node->left);
+        }
+        return node;
+    }
+
+    bool isOperatorStr(std::string str){
+        if(str == "+" || str == "-" || str == "*" || str == "/" || str =="(" || str == "^"){
+            return true;
+        }
+        return false;
+    }
+
+    Node *createNewNode(std::string value, Node *parent = nullptr, Node *left = nullptr, Node *right = nullptr){
+        Node *node = new Node();
+        node->value = value;
+        node->parent = parent;
+        node->left = left;
+        node->right = right;
+        return node;
+    }
+
+
+private:
+
+    int solveValue(std::string value,int lhs, int rhs){
+        if(value == "+"){
+            return add(lhs,rhs);
+        }else if(value == "-"){
+            return sub(lhs,rhs);
+        }else if(value == "*"){
+            return mul(lhs,rhs);
+        }else if(value == "/"){
+            return div(lhs,rhs);
+        }
+        return -1;
+    }
+
+    int add(int lhs, int rhs){
+        return lhs + rhs;
+    }
+
+    int sub(int lhs, int rhs){
+        return lhs - rhs;
+    }
+
+    int mul(int lhs, int rhs){
+        return lhs*rhs;
+    }
+
+    int div(int lhs, int rhs){
+        return lhs/rhs;
+    }
+
+private:
+    Node *_head;
+
+};
+
 class Calculator{
 
 public: // Constructors
     Calculator(){
-
+        _tree = new BinaryTree();
     }
 
 public: // Main Functions
 
 
     int calculateExpression(std::string userInput){
-        _userExpression = userInput;
+        _userInput = userInput;
+
         // Working at O(n)
         tokenizeStr();
         // Working at O(n)
@@ -37,27 +274,63 @@ public: // Main Functions
         // Working at O(n)
         constructExpression();
         // Working at O(n)
-        convertToPostfix();
-        // WOrking at O(n)
-        return solveExpression();
+        _userInput = convertToPrefix(_userInput);
+        // Working at O(n)
+        makeTree();
+
+        // Storing the answer
+        int value = _tree->solve();
+
+        // Erasing Values
+        truncateValue();
+        _tree->erase();
+
+        // Working at O(n)
+        return value;
     }
 
-    int calculateEquation(){
-        
-    }
 
 private: // auxillary functions
 
+    void truncateValue(){
+        _lTokenizeValue = std::vector<std::string>(); // left 
+        _rTokenizeValue = std::vector<std::string>(); // right
+        _lTokenizeOperator = std::vector<std::string>(); // left
+    }
+
+    void makeTree(){
+        std::string temp;
+        for(int i = 0; i < _userInput.length(); ++i){
+            char value = _userInput[i];
+            if(value == ';'){
+                _tree->pushValue(temp);
+                temp = "";
+            }else if(isOperator(value)){
+                temp = "";
+                temp+=value;
+                _tree->pushOperator(temp);
+                temp = "";
+            }else if(isVariable(value) || isDigit(value)){
+                temp += value;
+            }else if(value == '='){
+                temp = "";
+                temp+=value;
+                _tree->pushEqual(temp);
+                temp = "";
+            }
+        }
+    }
+
     void tokenizeStr(){
         std::string digitStr = "";
-        for(int i = 0; i < _userExpression.length(); ++i){
-            char input = _userExpression[i];
+        for(int i = 0; i < _userInput.length(); ++i){
+            char input = _userInput[i];
             if(isDigit(input)){
                 // accumulate the digit in a single string;
                 digitStr += input;
             }else if(isOperator(input)){
                 // push the digit
-                _tokenizeValue.push_back(digitStr + ';');
+                _lTokenizeValue.push_back(digitStr + ';');
 
                 // reset the digit
                 digitStr = "";
@@ -66,43 +339,29 @@ private: // auxillary functions
                 digitStr += input;
 
                 // push the operator
-                _tokenizeOperator.push_back(digitStr);
+                _lTokenizeOperator.push_back(digitStr);
 
                 // reset the digit
                 digitStr = "";
-            }else if(isVariable(input)){
-                // push the digits
-                _tokenizeValue.push_back(digitStr + ';');
-
-                // reset the digit
-                digitStr = "";
-
-                // put the variable in the digit (to avoid conversion)
-                digitStr += input;
-
-                // push the Variable
-                _tokenizeValue.push_back(digitStr);
-
-                // reset the digit
-                digitStr  = "";
             }else if(input == ' '){
                 // do nothing
-            }else{
-                std::cout << "Unknown Token: " << _userExpression[i] << std::endl;
+            }
+            else{
+                std::cout << "Unknown Token: " << _userInput[i] << std::endl;
                 exit(-1);
             }
         }
         if(digitStr.length()){
-            _tokenizeValue.push_back(digitStr +  ';');
+            _lTokenizeValue.push_back(digitStr +  ';');
         }
     }
 
     void addParenthesis(){
         bool previousMD = false;
-        for(int i = 0; i < _tokenizeOperator.size(); ++i){
-            if((_tokenizeOperator[i] == "*" || _tokenizeOperator[i] == "/") && previousMD == false){
-                _tokenizeValue[i] = '(' + _tokenizeValue[i];
-                _tokenizeValue[i+1] =  _tokenizeValue[i+1] + ')';
+        for(int i = 0; i < _lTokenizeOperator.size(); ++i){
+            if((_lTokenizeOperator[i] == "*" || _lTokenizeOperator[i] == "/") && previousMD == false){
+                _lTokenizeValue[i] = '(' + _lTokenizeValue[i];
+                _lTokenizeValue[i+1] =  _lTokenizeValue[i+1] + ')';
                 previousMD = true;
             }else{
                 previousMD = false;
@@ -112,20 +371,45 @@ private: // auxillary functions
 
     void constructExpression(){
         std::string tempExpression = "";
-        for(int valueIndex = 0, operatorIndex = 0; valueIndex < _tokenizeValue.size(); ++valueIndex){
+        for(int valueIndex = 0, operatorIndex = 0; valueIndex < _lTokenizeValue.size(); ++valueIndex){
             if(valueIndex >= 1){
-                tempExpression += _tokenizeOperator[operatorIndex++] + ' ';
+                tempExpression += _lTokenizeOperator[operatorIndex++] + ' ';
             }
-            tempExpression += _tokenizeValue[valueIndex] + ' ';
+            tempExpression += _lTokenizeValue[valueIndex] + ' ';
         }
-        _userExpression = tempExpression;
+        _userInput = tempExpression;
     }
 
-    void convertToPostfix(){
+    std::string convertToPrefix(std::string userInput){
+        int l = userInput.size();
+    
+        std::reverse(userInput.begin(), userInput.end());
+    
+        for (int i = 0; i < l; i++) {
+    
+            if (userInput[i] == '(') {
+                userInput[i] = ')';
+                i++;
+            }
+            else if (userInput[i] == ')') {
+                userInput[i] = '(';
+                i++;
+            }
+        }
+    
+        std::string prefix = convertToPostfix(userInput);
+    
+        // Reverse postfix
+        std::reverse(prefix.begin(), prefix.end());
+
+        return prefix;
+    }
+
+    std::string convertToPostfix(std::string userInput){
         std::stack<char> st;
         std::string result;
-        for (int i = 0; i < _userExpression.length(); i++) {
-            char c = _userExpression[i];
+        for (int i = 0; i < userInput.length(); i++) {
+            char c = userInput[i];
             if(c == ' '){
                 // do nothing
             }else if (isVariable(c) || isDigit(c) || c == ';'){
@@ -143,7 +427,7 @@ private: // auxillary functions
             }
             else {
                 while (!st.empty()
-                    && getPrecedency(_userExpression[i]) <= getPrecedency(st.top())) {
+                    && getPrecedency(userInput[i]) <= getPrecedency(st.top())) {
                     if (c == '^' && st.top() == '^')
                         break;
                     else {
@@ -158,14 +442,17 @@ private: // auxillary functions
             result += st.top();
             st.pop();
         }
-        _userExpression = result;
+        return result;
+    }
+
+    void convertToTree(){
     }
 
     int solveExpression(){
         std::stack<int> values;
         std::string tempValues;
-        for(int i = 0; i < _userExpression.length(); ++i){
-            char value = _userExpression[i];
+        for(int i = 0; i < _userInput.length(); ++i){
+            char value = _userInput[i];
             if(value == ';'){
                 try{
                     values.push(std::stoi(tempValues));
@@ -200,6 +487,7 @@ private: // auxillary functions
         values.pop();
         return result;
     }
+
 
 private: // auxillary functions
 
@@ -304,21 +592,29 @@ private: // operations
 
 private: // debug
     void print_token(){
-        for(int i = 0; i < _tokenizeValue.size(); ++i){
-            std::cout << _tokenizeValue[i] << ' ';
+        for(int i = 0; i < _lTokenizeValue.size(); ++i){
+            std::cout << _lTokenizeValue[i] << ' ';
         }
         std::cout << std::endl;
-        for(int i = 0; i < _tokenizeOperator.size(); ++i){
-            std::cout << _tokenizeOperator[i] << ' ';
+        for(int i = 0; i < _lTokenizeOperator.size(); ++i){
+            std::cout << _lTokenizeOperator[i] << ' ';
         }
         std::cout << std::endl;
-        std::cout << _userExpression;
+        std::cout << _userInput;
     }
 
 private: // Variables
-    std::vector<std::string> _tokenizeValue;
-    std::vector<std::string> _tokenizeOperator;
-    std::string _userExpression;
+
+    // expression variables
+    std::vector<std::string> _lTokenizeValue; // left 
+    std::vector<std::string> _rTokenizeValue; // right
+    std::vector<std::string> _lTokenizeOperator; // left
+
+
+    // the equation or expression
+    std::string _userInput;
+
+    BinaryTree *_tree;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 int main(){
     Calculator _calc;
-    std::string expression = "32 + 44 / 2 * 8 + 223 * 3";
-    int answer = _calc.calculateExpression(expression);
-    std::cout << expression << " = " << answer << std::endl;
+    std::string expression = "3x + 2 = 8";
+    std::cout << _calc.calculateExpression(expression) << "\n\n";
+    //while(expression != "0"){
+    //    std::cout << "> ";
+    //    std::getline(std::cin, expression);
+    //    std::cout << _calc.calculateExpression(expression) << "\n\n";
+    //}
     return 0;
 }
+
+
+
+// 3+(4/2)*8+(2*3)
+// +3+*/428*23
