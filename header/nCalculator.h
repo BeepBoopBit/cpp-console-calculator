36e5e4d9a684c57806806687df66b56209e15555
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2023-01-01 22:14:29 +0800

    Added temporary framework

--- header/nCalculator.h
+++ header/nCalculator.h
@@ -1,11 +1,39 @@
 #ifndef NCALCULATOR_H
 #define NCALCULATOR_H
 
+#include <string>
+#include <iostream>
+#include <sstream>
+
 class Calculator{
 public:
     Calculator(){
 
     }
+
+public:
+    void solve(std::string equation){
+
+        // Identify the missing variables
+        int count = identifyMissingVariables(equation);
+
+        // _simplexCalculator.solve(equation);
+
+
+
+    }
+
+private:
+    int identifyMissingVariables(std::string equation){
+        int count = 0;
+        for(int i = 0; i < equation.length(); i++){
+            if(equation[i] == 'x' || equation[i] == 'y' || equation[i] == 'z''){
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 };
 
